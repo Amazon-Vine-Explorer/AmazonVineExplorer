b4db7d39573a7fb85cc9979a63fc6c09fdef2ac4
Christof <114830546+Christof121@users.noreply.github.com>
2024-02-14 08:54:00 +0100

    Updatet Version

--- VineExplorer.user.js
+++ VineExplorer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Amazon Vine Explorer
 // @namespace    http://tampermonkey.net/
-// @version      0.10.6
+// @version      0.10.7
 // @updateURL    https://raw.githubusercontent.com/Amazon-Vine-Explorer/AmazonVineExplorer/main/VineExplorer.user.js
 // @downloadURL  https://raw.githubusercontent.com/Amazon-Vine-Explorer/AmazonVineExplorer/main/VineExplorer.user.js
 // @description  Better View, Search and Explore for Amazon Vine Products - Vine Voices Edition
