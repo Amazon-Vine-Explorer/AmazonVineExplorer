0b43dee3ca44816c4debf16e7d3b80504d6f2d93
Christof <114830546+Christof121@users.noreply.github.com>
2024-05-09 21:48:32 +0200

    Update globals.js

--- globals.js
+++ globals.js
@@ -26,6 +26,15 @@ const DATABASE_NAME = 'VineVoiceExplorer';
 const DATABASE_OBJECT_STORE_NAME = `${DATABASE_NAME}_Objects`;
 const DATABASE_VERSION = 2;
 
+// Make some things accessable from console
+unsafeWindow.ave = {
+    classes: [
+        DB_HANDLER = DB_HANDLER
+    ],
+    config: SETTINGS,
+    event: ave_eventhandler,
+};
+
 class AVE_EVENTHANDLER {
 
     /**
