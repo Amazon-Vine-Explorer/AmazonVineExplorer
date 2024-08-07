b05a53c378365700006499c1162ed8d88f47fab5
Werner Fleck <git@flexoft.net>
2024-08-07 09:06:41 +0200

    Added Deburau Tampermonkey script

---
+++ VineExplorerDeburauDevelopment.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Amazon Vine Explorer - Online Development Preview
+// @namespace    http://tampermonkey.net/
+// @version      9.9.9
+// @description  Better View, Search and Explore for Amazon Vine Products - Vine Voices Edition
+// @author       MarkusSR1984, Christof121, Deburau
+// @match        *://www.amazon.de/*
+// @match        *://www.amazon.com/*
+// @match        *://www.amazon.co.uk/*
+// @license      MIT
+// @icon         https://raw.githubusercontent.com/deburau/AmazonVineExplorer/main/vine_logo.png
+// @run-at       document-start
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM.getValue
+// @grant        GM.setValue
+// @grant        GM.xmlHttpRequest
+// @grant        unsafeWindow
+// @require      https://raw.githubusercontent.com/deburau/AmazonVineExplorer/main/globals.js
+// @require      https://raw.githubusercontent.com/deburau/AmazonVineExplorer/main/class_db_handler.js
+// @require      https://raw.githubusercontent.com/deburau/AmazonVineExplorer/main/class_product.js
+// @require      https://raw.githubusercontent.com/deburau/AmazonVineExplorer/main/VineExplorer.user.js
+// External Source
+// @require      https://raw.githubusercontent.com/eligrey/FileSaver.js/v2.0.4/src/FileSaver.js
+// @require      https://raw.githubusercontent.com/Christof121/VineFetchFix/main/fetchfix.js
+
+// ==/UserScript==
