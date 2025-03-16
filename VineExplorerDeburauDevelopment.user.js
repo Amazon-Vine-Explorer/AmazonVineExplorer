f85ec459bac7d7df9e13a52fb9310a83ece7c524
Werner Fleck <git@flexoft.net>
2025-03-17 00:18:59 +0100

    Next try

--- VineExplorerDeburauDevelopment.user.js
+++ VineExplorerDeburauDevelopment.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Amazon Vine Explorer
 // @namespace    http://tampermonkey.net/
-// @version      0.11.2.1
+// @version      0.11.2.2
 // @description  Better View, Search and Explore for Amazon Vine Products - Vine Voices Edition
 // @author       MarkusSR1984, Christof121, Olum-hack, Deburau
 // @match        *://www.amazon.de/*
