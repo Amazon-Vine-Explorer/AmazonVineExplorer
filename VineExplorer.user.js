e90739bcc21f9f924c49a5472dd7e622988abf15
adripo <26493496+adripo@users.noreply.github.com>
2025-04-01 22:44:09 +0200

    Update VineExplorer.user.js

--- VineExplorer.user.js
+++ VineExplorer.user.js
@@ -2461,28 +2461,28 @@ function initBackgroundScan() {
                 }
 
                 function _timeConversion(duration) {
-                  const portions = [];
-                
-                  const msInHour = 1000 * 60 * 60;
-                  const hours = Math.trunc(duration / msInHour);
-                  if (hours > 0) {
-                    portions.push(hours + 'h');
-                    duration = duration - (hours * msInHour);
-                  }
-                
-                  const msInMinute = 1000 * 60;
-                  const minutes = Math.trunc(duration / msInMinute);
-                  if (minutes > 0) {
-                    portions.push(minutes + 'm');
-                    duration = duration - (minutes * msInMinute);
-                  }
-                
-                  const seconds = Math.trunc(duration / 1000);
-                  if (seconds > 0) {
-                    portions.push(seconds + 's');
-                  }
-                
-                  return portions.join(' ');
+                    const portions = [];
+                    
+                    const msInHour = 1000 * 60 * 60;
+                    const hours = Math.trunc(duration / msInHour);
+                    if (hours > 0) {
+                        portions.push(hours + 'h');
+                        duration = duration - (hours * msInHour);
+                    }
+                    
+                    const msInMinute = 1000 * 60;
+                    const minutes = Math.trunc(duration / msInMinute);
+                    if (minutes > 0) {
+                        portions.push(minutes + 'm');
+                        duration = duration - (minutes * msInMinute);
+                    }
+                    
+                    const seconds = Math.trunc(duration / 1000);
+                    if (seconds > 0) {
+                        portions.push(seconds + 's');
+                    }
+                    
+                    return portions.join(' ');
                 }
             }
         }
